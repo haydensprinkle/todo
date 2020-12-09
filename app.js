604dd1873f53f9c759e3249b9a3bbd4b0d69dafd
haydensprinkle <haydensprinkle@gmail.com>
2020-12-09 10:25:11 -0800

    fixed new task generation functionality/added padding on li elements

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const taskList = document.querySelector(".task-list");
 
 //functions
 let newTask = (event) => {
+  event.preventDefault();
   //create new task Div
   const taskWrapper = document.createElement("div");
   taskWrapper.classList.add("task-wrapper");
   //create LI
   const newTask = document.createElement("li");
-  newTask.innerHTML = todoText.nodeValue;
+  newTask.innerHTML = todoText.value;
+  todoText.value = "";
   newTask.classList.add("task");
   taskWrapper.appendChild(newTask);
 
