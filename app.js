c8d64fb2e50972dd109492923245cb9924e14a46
haydensprinkle <haydensprinkle@gmail.com>
2021-01-01 17:58:32 -0800

    scrapped styles, updated functionality

--- app.js
+++ app.js
@@ -16,9 +16,40 @@ let newTask = (event) => {
   newTask.classList.add("task");
   taskWrapper.appendChild(newTask);
 
+  //create check button
+  const checkButton = document.createElement("input");
+  checkButton.type = "checkbox";
+  checkButton.value = "Done";
+  checkButton.id = "checkButton";
+  checkButton.name = "checkButton";
+  checkButton.classList.add("check-button");
+  const checkButtonLabel = document.createElement("label");
+  checkButtonLabel.for = "checkButton";
+  checkButtonLabel.innerText = "Done";
+  taskWrapper.appendChild(checkButton);
+  taskWrapper.appendChild(checkButtonLabel);
+  //create delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.classList.add("delete-button");
+  taskWrapper.appendChild(deleteButton);
   //Append to list
   taskList.appendChild(taskWrapper);
+  
+  checkButton.addEventListener('change', function() {
+    if (this.checked) {
+      newTask.classList.replace("task","completed-task");
+    } else {
+      newTask.classList.replace("completed-task","task");
+    }
+  });
+
+  deleteButton.addEventListener('click', function(){
+    taskWrapper.remove();
+  });
+
 };
 
 //event listeners
 addButton.addEventListener("click", newTask);
+
